57bb68e34e336ba614d6bb638ef4e375d64b7129
wellingtonwebber <wellingtonwebber22@gmail.com>
2024-07-21 19:52:13 -0300

    refactoring motor route

---
+++ src/routes/create-motor.ts
@@ -0,0 +1,61 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import { z } from "zod"
+import { prisma } from "../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function createMotor(app: FastifyInstance){
+    app
+        .withTypeProvider<ZodTypeProvider>()
+        .post('/motors', {
+            schema: {
+                body: z.object({
+                    code: z.string(),
+                    manufacturer: z.string().nullable(),
+                    voltage: z.number().int().positive(),
+                    current: z.number().positive(),
+                    rpm: z.number().int().positive(),
+                    frame: z.string().nullable(),
+                    type: z.string().nullable(),
+                    model: z.string().nullable(),
+                    status: z.string()
+                }),
+                response: {
+                    201: z.object({
+                        motorId: z.string().uuid()
+                    })
+                }
+            }
+
+        }, async (request, reply) => {
+
+            const data = request.body
+
+            const motorWithSameCode = await prisma.motor.findUnique({
+                where: {
+                    code: data.code
+                }
+            })
+
+            if (motorWithSameCode !==null) {
+                throw new Error('Motor with same code already exists')
+            }
+
+            const motor = await prisma.motor.create({
+                data: {
+                    code: data.code,
+                    manufacturer: data.manufacturer,
+                    voltage: data.voltage,
+                    current: data.current,
+                    rpm: data.rpm,
+                    frame: data.frame,
+                    type: data.type,
+                    model: data.model,
+                    status: data.status
+                }
+            })
+
+            return reply.status(201).send({motorId: motor.id})
+
+        })
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,13 @@
 import fastify from "fastify";
-import { z } from 'zod';
-import { PrismaClient } from '@prisma/client'
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createMotor } from "./routes/create-motor";
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-    log:['query'],
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.get('/', () => {
-    return 'Hello world!'
-})
-
-app.post('/motors', async (request, reply) => {
-
-    const createMotorSchema = z.object({
-        code: z.string(),
-        manufacturer: z.string().nullable(),
-        voltage: z.number().int().positive(),
-        current: z.number().positive(),
-        rpm: z.number().int().positive(),
-        frame: z.string().nullable(),
-        type: z.string().nullable(),
-        model: z.string().nullable(),
-        status: z.string()
-    })
-
-    const data = createMotorSchema.parse(request.body)
-
-    const motor = await prisma.motor.create({
-        data: {
-            code: data.code,
-            manufacturer: data.manufacturer,
-            voltage: data.voltage,
-            current: data.current,
-            rpm: data.rpm,
-            frame: data.frame,
-            type: data.type,
-            model: data.model,
-            status: data.status
-        }
-    })
-
-    return reply.status(201).send({motorId: motor.id})
-
-})
+app.register(createMotor)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
